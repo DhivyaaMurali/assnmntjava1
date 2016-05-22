64270e44772a9d504c1176be5b03feecaa2638e9
DhivyaaMurali <dhivshan86@gmail.com>
2016-05-22 22:41:53 +0530

    Add files via upload

---
+++ Operationonvariables.java
@@ -0,0 +1,18 @@
+/* This Java file does various operations on variables
+ * 
+ */package assignment1;
+ 
+
+public class Operationonvariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=5,y=10;
+System.out.println(x+y*2);
+System.out.println(x-y+2);
+System.out.println((x+y)*2);
+System.out.println(y%x);
+
+	}
+
+}
